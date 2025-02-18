7e142b79acdb3b86acc780007605c9b58a9c78f6
Rafael Mattos <rafosomattos@gmail.com>
2025-02-17 21:38:55 -0300

    Criado projeto de controle de eventos

---
+++ src/main/java/com/evento/resources/UsuarioResource.java
@@ -0,0 +1,35 @@
+package com.evento.resources;
+
+import com.evento.dtos.UsuarioDTO;
+import com.evento.models.Usuario;
+import com.evento.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> buscarUsuarioPorId(@PathVariable Long id){
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorId(id));
+
+    }
+
+    @PostMapping()
+    public ResponseEntity<UsuarioDTO> cadastrarUsuario(@RequestBody UsuarioDTO usuarioDTO){
+        UsuarioDTO usuario = usuarioService.cadastrarUsuario(usuarioDTO);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> deletarUsuario(@PathVariable Long id){
+        UsuarioDTO usuario = usuarioService.deletarUsuario(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+}
